3299cafc09f07db6caa3626c762abbc7786be68f
ismail aybars yılmaz <56890557+itucsdb1906@users.noreply.github.com>
2019-11-12 19:49:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
-    return "yilmaz"
+    #return "yilmaz"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
